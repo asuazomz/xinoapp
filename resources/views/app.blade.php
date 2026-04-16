feb164087ef221c6dda0989ed9037e47655c1172
Andrés Suazo <andressm@MacBook-Air-de-Andres.local>
2026-04-16 16:27:17 -0400

    Migracion de Breeze y ajuste de rutas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,33 +1,15 @@
-<!DOCTYPE HTML>
-<HTML lang= "es">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name ="viewport" content ="width=device-width, initial-scale=1.0">
-    <title> La xinoapp</title>
-    <style>
-    body{
-        background: black;
-            color: white;
-            text-align: center;
-            font-family: Arial, sans-serif;
-            padding-top: 50px;
-    }
-    a, button {
-            padding: 10px 20px;
-            margin: 10px;
-            border: none;
-            background: white;
-            color: black;
-            text-decoration: none;
-            cursor: pointer;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    @vite(['resources/js/app.ts'])
+    @inertiaHead
 </head>
-<body>
-    <h1>Aprendiendo a hacer app Laravel</h1>
-    <p>les go</p>
-    <a href="/login"> Ingresar </a>
-    
-    <a href="/register">Registrar Usuario </a>
+<body class="font-sans antialiased">
+    @inertia
 </body>
 </html>
